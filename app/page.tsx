5e5233bcd3aadb7f043edd84db80c05df0e394ed
moussa <dd@>
2026-01-12 09:12:42 +0100

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main>
       <h1>Meal Booking</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
